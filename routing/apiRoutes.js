c80f7c3469fece8b7fad47de8fbb87b847d3b902
Levi Crouch <levi.crouch@gmail.com>
2018-01-11 21:55:08 -0800

    updates

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -2,6 +2,7 @@
 // pull in the objFriends from ../data/friends file
 var objFriends = require("../app/data/friends");
 // console.log("objFriends",objFriends);
+
 ///////////////////////////////////////////////////////////////////
 // ROUTES
 ///////////////////////////////////////////////////////////////////
@@ -13,16 +14,26 @@ module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
         res.json(objFriends);
     });
-    // TODO POST routes to "api/friends". This will be used to handle incoming survey results. 
-    // Also used for handling compatibility logic
+
     app.post("/api/friends", function (req, res) {
-        var body = req.body;
-        objFriends.push(body);
-        console.log(body);
+        // TODO POST routes to "api/friends". This will be used to handle incoming survey results. 
+        // Also used for handling compatibility logic
+        // objFriends.push(body);
+        console.log("req.body.scores",req.body.scores);
+
+        // Determine which person is the best match
+        // Working with the incoming data array "body.scores"
+        // var incomingScore = [];
+        // for(var i = 0; i < req.body.scores.length; i++){
+        //     console.log("objFriends[i]",objFriends[i]);
+        //     // objFriends[i].score.push(objFriends[i].scores[i]);
+        // }
+        // console.log(typeof incomingScore);
+        // console.log("incomingScore",incomingScore);
+        // Send back matching data
         res.json({
             "success": true,
-            "data": body,
-            "object": objFriends
+            "data": req.body
         });
     });
 };
